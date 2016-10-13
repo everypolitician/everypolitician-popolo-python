dba8ce8453fa42b06965c239f5fdd3935726916c
Mark Longair <mhl@pobox.com>
2016-10-13 10:35:03 +0100

    Include the README.rst as the long_description for PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+from os.path import join, dirname
+
+with open(join(dirname(__file__), 'README.rst')) as f:
+    readme_text = f.read()
+
 setup(
     name = "everypolitician-popolo",
     version = "0.0.1",
@@ -6,6 +11,7 @@ setup(
     author = "Mark Longair",
     author_email = "mark@mysociety.org",
     description = "Parse and model Popolo data from EveryPolitician",
+    long_description = readme_text,
     license = "AGPL",
     keywords = "politics data civic-tech",
     install_requires = [
