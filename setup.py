82fb71ad9803ea3475e0079d2d0c7e0f2c789d1e
Mark Longair <mhl@pobox.com>
2016-10-28 15:07:32 +0100

    Bump version to 0.0.7 and update the changelog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(join(dirname(__file__), 'README.rst')) as f:
 
 setup(
     name = "everypolitician-popolo",
-    version = "0.0.6",
+    version = "0.0.7",
     packages = find_packages(),
     author = "Mark Longair",
     author_email = "mark@mysociety.org",
